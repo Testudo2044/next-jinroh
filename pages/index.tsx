61be034152eba3754e6b21d5407231510fdb4d1f
Testudo2044 <tessoualle@gmail.com>
2021-11-09 04:35:04 +0900

    変遷

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,17 +2,17 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Si-cle</title>
-        <meta name="description" content="サイクルではあなたのサイトを制作します" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main>
-      </main>
+    <div className="bg-yellow-300">
+      <h1 className="text-3xl">
+        Read{' '}
+        <Link href="/posts/post">
+          <a className="text-blue-500 text-3xl">this page!</a>
+        </Link>
+      </h1>
     </div>
   )
 }
---
+++ pages/posts/post.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+export default function FirstPost() {
+  return (
+    <>
+      <h1 className="text-3xl">First Post</h1>
+      <h2 className="text-2xl">
+        <Link href="/">
+          <a className="text-blue-500">Back to home</a>
+        </Link>
+      </h2>
+    </>
+  )
+}
