ba076c137b3b861afc0cc0dd1f28bf1caadde1db
Testudo2044 <tessoualle@gmail.com>
2021-12-11 10:33:23 +0900

    ruby

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,12 +13,36 @@ const HeaderFunction: React.FC = () =>
     <div className="PCStyle hidden lg:block">
       <div className="bg-black ">
         <ul className="text-2xl text-white flex">
-          <li className="lg:hover:text-3xl"><a href=""><ruby><rb>ABOUT</rb><rt>UN人狼とは</rt></ruby></a></li>
-          <li className="lg:hover:text-3xl"><a href=""><ruby><rb>RULE</rb><rt>ゲームルール</rt></ruby></a></li>
-          <li className="lg:hover:text-3xl"><a className=" " href=""><ruby><rb>SET</rb><rt>販売</rt></ruby></a></li>
-          <li className="lg:hover:text-3xl"><a href=""><ruby><rb>CHARACTER</rb><rt>役職紹介</rt></ruby></a></li>
-          <li className="lg:hover:text-3xl"><a href=""><ruby><rb>MEMBER</rb><rt>メンバー</rt></ruby></a></li>
-          <li className="lg:hover:text-3xl"><a href=""><ruby><rb>COMTACT</rb><rt>お問い合わせ</rt></ruby></a></li>
+          <li className="lg:hover:text-3xl">
+            <Link href="/">
+              <a><ruby>ABOUT<rt>UN人狼とは</rt></ruby></a>
+            </Link>
+          </li>
+          <li className="lg:hover:text-3xl">
+            <Link href="/">
+            <a><ruby>RULE<rt>ゲームルール</rt></ruby></a>
+          </Link>
+          </li>
+          <li className="lg:hover:text-3xl">
+            <Link href="/">
+              <a><ruby>SET<rt>販売</rt></ruby></a>
+            </Link>
+          </li>
+          <li className="lg:hover:text-3xl">
+            <Link href="/">
+              <a><ruby>CHARACTER<rt>役職紹介</rt></ruby></a>
+            </Link>
+          </li>
+          <li className="lg:hover:text-3xl">
+            <Link href="/">
+              <a><ruby>MEMBER<rt>メンバー</rt></ruby></a>
+            </Link>
+          </li>
+          <li className="lg:hover:text-3xl">
+            <Link href="/">
+              <a><ruby>COMTACT<rt>お問い合わせ</rt></ruby></a>
+            </Link>
+          </li>
         </ul>
       </div>
     </div>
@@ -31,7 +55,7 @@ const MainFunction: React.FC = () =>
 
 const FooterFunction: React.FC = () =>
   <div>
-
+    <h1>hogehoge</h1>
   </div>;
 
 
--- pages/posts/top.tsx
+++ pages/posts/top.tsx
@@ -1,12 +1,10 @@
 import Link from 'next/link'
 import Head from 'next/head'
-import Layout from '../components/layout'
-import Header from '../components/Header'
 
 export default function FirstPost() {
   return (
     <>
-      
+
       <h1 className="text-3xl">First Post</h1>
       <h2 className="text-2xl">
         <Link href="/">
